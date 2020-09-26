4cadb6f31298f104971bcd9d4fbabe3441f00d50
Ned Ruggeri <ruggeri@self-loop.com>
2020-09-25 22:35:28 -0700

    don't need to color us/japan for no reason :-P

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import { Game } from "./game";
 
 async function main() {
   const worldMap = await awaitWorldMapLoad();
-  // worldMap.setCountryColor("us", "yellow");
-  // worldMap.setCountryColor("jp", "red");
   new Game(worldMap);
 }
 
