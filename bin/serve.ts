961f1d651056c114ee9bf66323cb7c45f01ded32
Ned Ruggeri <ruggeri@self-loop.com>
2023-04-17 16:12:44 -0700

    make serve.ts a little more async

--- bin/serve.ts
+++ bin/serve.ts
@@ -6,75 +6,91 @@ import express from "express";
 import os from "os";
 import { CountrySuccessStatistics } from "../src/country_success_statistics";
 
-const app = express();
+async function main() {
+  const app = express();
 
-app.use(express.static("./dist/"));
-app.use(bodyParser.json());
+  app.use(express.static("./dist/"));
+  app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.sendFile("./dist/index.html");
-});
+  app.get("/", (_, res) => {
+    res.sendFile("./dist/index.html");
+  });
 
-app.get("/bundle.js", (req, res) => {
-  res.sendFile("./dist/bundle.js");
-});
+  app.get("/bundle.js", (_, res) => {
+    res.sendFile("./dist/bundle.js");
+  });
 
-app.get("/World Map.svg", (req, res) => {
-  res.sendFile("./dist/World Map.svg");
-});
+  app.get("/World Map.svg", (_, res) => {
+    res.sendFile("./dist/World Map.svg");
+  });
 
-const username = os.userInfo().username;
-const statsFilename = `./dist/${username}-country-success-statistics.json`;
-let countrySuccessStatisticsMap: Record<string, CountrySuccessStatistics> = {};
-try {
-  countrySuccessStatisticsMap = JSON.parse(
-    fs.readFileSync(statsFilename, { encoding: "utf-8" })
-  );
-} catch (e) {
-  console.log(e);
-  countrySuccessStatisticsMap = {};
+  const username = os.userInfo().username;
+  const statsFilename = `./dist/${username}-country-success-statistics.json`;
+  type CountrySuccessStatisticsMap = Record<string, CountrySuccessStatistics>;
+
+  async function readCountryStatistics(): Promise<CountrySuccessStatisticsMap> {
+    try {
+      return JSON.parse(
+        await fs.promises.readFile(statsFilename, { encoding: "utf-8" })
+      );
+    } catch (err) {
+      console.log(err);
+      return {};
+    }
+  }
+
+  async function writeCountryStatistics(
+    countrySuccessStatisticsMap: CountrySuccessStatisticsMap
+  ): Promise<undefined> {
+    try {
+      await fs.promises.writeFile(
+        statsFilename,
+        JSON.stringify(countrySuccessStatisticsMap, null, 2)
+      );
+
+      return;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+
+  let countrySuccessStatisticsMap = await readCountryStatistics();
+
+  app.get("/country-success-statistics", async (_, res) => {
+    res.json(countrySuccessStatisticsMap);
+  });
+
+  app.post("/country-success-statistics", async (req, res) => {
+    console.log(req.body);
+
+    let countrySuccessStatistics =
+      countrySuccessStatisticsMap[req.body.isoCountryCode];
+    if (countrySuccessStatistics === undefined) {
+      countrySuccessStatistics = {
+        isoCountryCode: req.body.isoCountryCode,
+        timesGuessedCorrectly: 0,
+        timesGuessedIncorrectly: 0,
+      };
+      countrySuccessStatisticsMap[req.body.isoCountryCode] =
+        countrySuccessStatistics;
+    }
+
+    if (req.body.direction === "correct") {
+      countrySuccessStatistics.timesGuessedCorrectly++;
+    } else if (req.body.direction === "incorrect") {
+      countrySuccessStatistics.timesGuessedIncorrectly++;
+    }
+
+    await writeCountryStatistics(countrySuccessStatisticsMap);
+
+    res.json(countrySuccessStatistics);
+  });
+
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`App is listening on port ${port}.`);
+  });
 }
 
-app.get("/country-success-statistics", async (_, res) => {
-  res.json(countrySuccessStatisticsMap);
-});
-
-app.post("/country-success-statistics", async (req, res) => {
-  console.log(req.body);
-
-  let countrySuccessStatistics =
-    countrySuccessStatisticsMap[req.body.isoCountryCode];
-  if (countrySuccessStatistics === undefined) {
-    countrySuccessStatistics = {
-      isoCountryCode: req.body.isoCountryCode,
-      timesGuessedCorrectly: 0,
-      timesGuessedIncorrectly: 0,
-    };
-    countrySuccessStatisticsMap[
-      req.body.isoCountryCode
-    ] = countrySuccessStatistics;
-  }
-
-  if (req.body.direction === "correct") {
-    countrySuccessStatistics.timesGuessedCorrectly++;
-  } else if (req.body.direction === "incorrect") {
-    countrySuccessStatistics.timesGuessedIncorrectly++;
-  }
-
-  fs.writeFile(
-    statsFilename,
-    JSON.stringify(countrySuccessStatisticsMap, null, 2),
-    (err) => {
-      if (err) {
-        console.log(err);
-      }
-    }
-  );
-
-  res.json(countrySuccessStatistics);
-});
-
-const port = 8080;
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}.`);
-});
+main();
