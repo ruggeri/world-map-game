099a363f1d2c4442e62d7e9bb9f19babdc5c01f0
Ned Ruggeri <ruggeri@self-loop.com>
2023-04-18 19:07:03 -0700

    make it easier to debug by giving access to App state

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,30 @@ import CountrySuccessStatisticsMap from "./country_success_statistics";
 import Game from "./game";
 import WorldMap from "./world_map";
 
+interface App {
+  allDataMap: CountryDataMap;
+  countrySuccessStatisticsMap: CountrySuccessStatisticsMap;
+  worldMap: WorldMap;
+  game: Game;
+}
+
+declare global {
+  var app: App;
+}
+
 async function main() {
   const allDataMap = CountryDataMap.allDataMap();
   const countrySuccessStatisticsMap =
     await CountrySuccessStatisticsMap.fetchMapFromServer();
   const worldMap = await WorldMap.fetchMapFromServer();
-  new Game(allDataMap, countrySuccessStatisticsMap, worldMap);
+  const game = new Game(allDataMap, countrySuccessStatisticsMap, worldMap);
+
+  window.app = {
+    allDataMap,
+    countrySuccessStatisticsMap,
+    worldMap,
+    game,
+  };
 }
 
 main().catch((err) => {
